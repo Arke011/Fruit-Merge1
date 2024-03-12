d1c6f42bb39b74c9f6a0d2e3670059258908a173
Arke011 <146362737+Arke011@users.noreply.github.com>
2024-03-12 21:51:37 +0200

    fixedMerging, added cute apple sprite

--- Assets/Scripts/FruitMerge.cs
+++ Assets/Scripts/FruitMerge.cs
@@ -5,7 +5,13 @@ public class FruitMerge : MonoBehaviour
 {
     private bool inAir = true;
     private bool canDrop = false;
+    public GameManager manager;
 
+
+    void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
     void Update()
     {
         if (inAir == true)
@@ -13,17 +19,29 @@ public class FruitMerge : MonoBehaviour
             canDrop = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop == true)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop)
         {
+
+            
+            
             inAir = false;
             canDrop = false;
             GetComponent<Rigidbody2D>().gravityScale = 3;
 
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePosition.x, transform.position.y, transform.position.z);
+            manager.movementFruit.position = new Vector2(mousePosition.x, manager.movementFruit.position.y);
+
+
             GameManager.canSpawn = true;
+           
+            
+
+
+
         }
     }
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,7 +51,9 @@ public class FruitMerge : MonoBehaviour
             GameManager.mergeFruit = true;
             GameManager.whichFruit = int.Parse(gameObject.tag);
             Destroy(gameObject);
+            
         }
     }
-}
 
+   
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,12 @@ public class GameManager : MonoBehaviour
     static public Vector2 spawnPos;
     static public bool mergeFruit = false;
     static public int whichFruit;
+    public Transform movementFruit;
+    public Transform mergedFruit;
+
+    
+    
+    
 
     void Start()
     {
@@ -22,7 +28,7 @@ public class GameManager : MonoBehaviour
 
     void spawnFruit()
     {
-        if (canSpawn == true)
+        if (canSpawn)
         {
             StartCoroutine(spawnTimer());
             canSpawn = false;
@@ -31,17 +37,30 @@ public class GameManager : MonoBehaviour
 
     void mergeFruits()
     {
-        if (mergeFruit == true)
+        if (mergeFruit)
         {
             mergeFruit = false;
-            Instantiate(fruits[whichFruit + 1], spawnPos, fruits[0].rotation);
+            mergedFruit = Instantiate(fruits[whichFruit + 1], spawnPos, fruits[0].rotation);
+
+            // Assuming mergedFruit has a Rigidbody2D component
+            Rigidbody2D mergedFruitRb = mergedFruit.GetComponent<Rigidbody2D>();
+
+            if (mergedFruitRb != null)
+            {
+                // Set the gravity scale to 3
+                mergedFruitRb.gravityScale = 3f;
+            }
+            else
+            {
+                Debug.LogWarning("The mergedFruit object doesn't have a Rigidbody2D component.");
+            }
         }
     }
 
-    IEnumerator spawnTimer()
+
+    public IEnumerator spawnTimer()
     {
         yield return new WaitForSeconds(0.75f);
-        Instantiate(fruits[Random.Range(0, 4)], transform.position, fruits[0].rotation);
-        
+        movementFruit = Instantiate(fruits[Random.Range(0, 4)], transform.position, fruits[0].rotation);      
     }
 }
